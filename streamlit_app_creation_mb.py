70217f24a66c036ed6d6fceeae74e687bc499d16
learning-again-mb <2024dc04126@wilp.bits-pilani.ac.in>
2026-01-12 10:04:33 -0500

    Update streamlit_app_creation_mb.py

--- streamlit_app_creation_mb.py
+++ streamlit_app_creation_mb.py
@@ -2,6 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import joblib
+import matplotlib.pyplot as plt
 from pathlib import Path
 
 # =========================================================
